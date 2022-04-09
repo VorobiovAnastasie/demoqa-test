c5de5d13b80770c8183908f73dddfe1d6d09fb11
Anastasie Vorobiova <vorobiovanastasie@gmail.com>
2022-04-09 13:52:06 +0200

    homework fill student form

---
+++ src/test/java/com/demoqa/HomeWork.java
@@ -0,0 +1,46 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class HomeWork {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+
+        $("[id=firstName]").setValue("Harry");
+        $("[id=lastName]").setValue("Potter");
+        $("[id=userEmail]").setValue("harry@potter.com");
+        $(new ByText("Male")).click();
+        $("[id=userNumber]").setValue("3807645322");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1997");
+        $("[aria-label$='September 19th, 1997']").click();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $( new ByText("Reading")).click();
+        //$("#uploadPicture").uploadFromClasspath("photo_2022-02-15_14-07-18.jpg");
+        $("#currentAddress").setValue("Nyutona street, building 5, apt 95");
+        $("#stateCity-wrapper").$("input").setValue("NCR").sendKeys(Keys.TAB);
+        $("#city").$("input").setValue("Delhi").sendKeys(Keys.TAB);
+        $("#submit").click();
+        $(".modal-title").shouldHave(Condition.text("Thanks for submitting the form"));
+    }
+
+
+}
