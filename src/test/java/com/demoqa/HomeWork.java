7cf13827e00188bda4812430525b8b2fe9684b8a
Anastasie Vorobiova <vorobiovanastasie@gmail.com>
2022-04-11 20:36:19 +0200

    changing of pic format

--- src/test/java/com/demoqa/HomeWork.java
+++ src/test/java/com/demoqa/HomeWork.java
@@ -34,7 +34,7 @@ public class HomeWork {
         $("[aria-label$='September 19th, 1997']").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $( new ByText("Reading")).click();
-        $("#uploadPicture").uploadFromClasspath("Harry_Potter.webp");
+        $("#uploadPicture").uploadFromClasspath("Harry_Potter.jpeg");
         $("#currentAddress").setValue("Nyutona street, building 5, apt 95");
         $("#stateCity-wrapper").$("input").setValue("NCR").sendKeys(Keys.TAB);
         $("#city").$("input").setValue("Delhi").sendKeys(Keys.TAB);
@@ -47,7 +47,7 @@ public class HomeWork {
         $(".table-responsive").$(ByText("Date of Birth")).parent().shouldHave(text("19 September 1997"));
         $(".table-responsive").$(ByText("Subjects")).parent().shouldHave(text("English"));
         $(".table-responsive").$(ByText("Hobbies")).parent().shouldHave(text("Reading"));
-        $(".table-responsive").$(ByText("Picture")).parent().shouldHave(text("Harry_Potter.webp"));
+        $(".table-responsive").$(ByText("Picture")).parent().shouldHave(text("Harry_Potter.jpeg"));
         $(".table-responsive").$(ByText("Address")).parent().shouldHave(text("Nyutona street, building 5, apt 95"));
         $(".table-responsive").$(ByText("State and City")).parent().shouldHave(text("NCR Delhi"));
     }
