174630dccd3c48df32d9db3b7dabb68ee327f573
Anastasie Vorobiova <vorobiovanastasie@gmail.com>
2022-04-11 20:24:55 +0200

    added picture and code for the final registration form check

--- src/test/java/com/demoqa/HomeWork.java
+++ src/test/java/com/demoqa/HomeWork.java
@@ -34,13 +34,24 @@ public class HomeWork {
         $("[aria-label$='September 19th, 1997']").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $( new ByText("Reading")).click();
-        //$("#uploadPicture").uploadFromClasspath("photo_2022-02-15_14-07-18.jpg");
+        $("#uploadPicture").uploadFromClasspath("Harry_Potter.webp");
         $("#currentAddress").setValue("Nyutona street, building 5, apt 95");
         $("#stateCity-wrapper").$("input").setValue("NCR").sendKeys(Keys.TAB);
         $("#city").$("input").setValue("Delhi").sendKeys(Keys.TAB);
         $("#submit").click();
         $(".modal-title").shouldHave(Condition.text("Thanks for submitting the form"));
+        $(".table-responsive").$(ByText("Student Name")).parent().shouldHave(text("Harry Potter"));
+        $(".table-responsive").$(ByText("Student Email")).parent().shouldHave(text("harry@potter.com"));
+        $(".table-responsive").$(ByText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(ByText("Mobile")).parent().shouldHave(text("3807645322"));
+        $(".table-responsive").$(ByText("Date of Birth")).parent().shouldHave(text("19 September 1997"));
+        $(".table-responsive").$(ByText("Subjects")).parent().shouldHave(text("English"));
+        $(".table-responsive").$(ByText("Hobbies")).parent().shouldHave(text("Reading"));
+        $(".table-responsive").$(ByText("Picture")).parent().shouldHave(text("Harry_Potter.webp"));
+        $(".table-responsive").$(ByText("Address")).parent().shouldHave(text("Nyutona street, building 5, apt 95"));
+        $(".table-responsive").$(ByText("State and City")).parent().shouldHave(text("NCR Delhi"));
     }
+
 
 
 }
