c0cc276af05f7c22cf0cacd7a1471a8b8c535ec7
Anastasie Vorobiova <vorobiovanastasie@gmail.com>
2022-04-26 23:29:23 +0300

    homework page,faker etc

---
+++ src/test/java/pages/RegistrationFormPage.java
@@ -0,0 +1,97 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import components.CalendarComponent;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public  class RegistrationFormPage {
+    CalendarComponent calendar = new CalendarComponent();
+
+    // locators
+    SelenideElement firstNameInput = $("#firstName");
+    SelenideElement lastNameInput = $("#lastName");
+    SelenideElement userEmailInput = $("#userEmail");
+    SelenideElement subjectsInput = $("#subjectsInput");
+
+    //actions
+    public RegistrationFormPage openPage() {
+        open("/automation-practice-form");
+        return this;
+    }
+
+    public RegistrationFormPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationFormPage setLastName(String value) {
+        lastNameInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationFormPage setEmail(String value) {
+        userEmailInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationFormPage setGender(String value) {
+        $("#genterWrapper").$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationFormPage setBirthDate(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        calendar.setDate(day, month, year);
+        return this;
+    }
+
+    public RegistrationFormPage setUserNumber(String value) {
+        $("#userNumber").setValue(value);
+        return this;
+    }
+    public RegistrationFormPage subjectsInput(String value) {
+        subjectsInput.setValue(value).pressEnter();
+        return this;
+    }
+
+    public RegistrationFormPage hobbiesInput(String value) {
+        $(byText("Reading")).click();
+        return this;
+    }
+
+    public RegistrationFormPage uploadPicture(String value) {
+        $("#uploadPicture").uploadFile(new File("src/test/resources/Harry_Potter.jpeg"));
+        return this;
+    }
+
+    public RegistrationFormPage currentAddress(String value) {
+        $("#currentAddress").setValue(value);
+        return this;
+    }
+    public RegistrationFormPage selectState(String value) {
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        return this;
+    }
+    public RegistrationFormPage selectCity(String value) {
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        return this;
+    }
+
+    public RegistrationFormPage submit() {
+        $("#submit").click();
+        return this;
+    }
+
+    public RegistrationFormPage checkResult(String key, String value) {
+        $(".table-responsive").$(byText(key))
+                .parent().shouldHave(text(value));
+        return this;
+    }
+}
